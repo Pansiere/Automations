be176b5ef7bdf0734081a091d5b580fb00cf4c9c
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-18 17:11:13 -0300

    update's update

--- zsh/update.sh
+++ zsh/update.sh
@@ -4,4 +4,4 @@ rm ~/.zshrc
 mv /tmp/Automations/zsh/.zshrc ~/.zshrc
 rm -rf /tmp/Automations
 
-echo "Pansiere - Configuracoes concluidas com sucesso. Reinicie o terminal para aplicar."
+echo "Configuracoes concluidas com sucesso. - #Pansiere"
