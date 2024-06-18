fd6d1f19f7ad3ffea953b2a753a04a4b0d11217a
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-18 16:18:36 -0300

    git auto pull

--- git auto pull/git_auto_pull.sh
+++ git auto pull/git_auto_pull.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git clone git@github.com:Pansiere/Automations.git
-
 mv Automations/git\ auto\ pull/.git_check_updates.sh  .
 mv Automations/git\ auto\ pull/.git_pull.sh .
 rm -rf Automations
