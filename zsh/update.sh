ed0fe478f527241dc0823be807f182baff69ab5a
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-18 16:49:18 -0300

    git auto pull

--- zsh/update.sh
+++ zsh/update.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ~/
-
-rm .zshrc
-mv ~/Automations/zsh/.zshrc ~/.zshrc
-rm -rf Automations
+rm ~/.zshrc
+mv /tmp/Automations/zsh/.zshrc ~/.zshrc
+rm -rf /tmp/Automations
 
 echo "Pansiere - Configuracoes concluidas com sucesso. Reinicie o terminal para aplicar."
