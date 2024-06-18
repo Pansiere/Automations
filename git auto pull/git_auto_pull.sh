5ecda99d069ab8421e8b58fd83bea4f6439aa118
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-18 16:23:03 -0300

    git auto pull

--- git auto pull/git_auto_pull.sh
+++ git auto pull/git_auto_pull.sh
@@ -2,6 +2,8 @@
 
 mv Automations/git\ auto\ pull/.git_check_updates.sh  .
 mv Automations/git\ auto\ pull/.git_pull.sh .
+chmod +x .git_check_updates.sh
+chmod +x .git_pull.sh
 rm -rf Automations
 
 echo "Configuracoes concluidas com sucesso. - #Pansiere -"
