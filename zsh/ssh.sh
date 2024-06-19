8052a285ce8c5637c4186a35ff4a44783ec0c555
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-06-19 13:19:46 -0300

    Update

--- zsh/ssh.sh
+++ zsh/ssh.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-cd ~/
-
-git clone git@github.com:Pansiere/ssh.git
-
 rm ~/.ssh/authorized_keys
-mv ~/ssh/authorized_keys ~/.ssh/
-rm -rf Automations
-rm -rf ssh
+mv /tmp/ssh/authorized_keys ~/.ssh/
+rm -rf /tpm/Automations
+rm -rf /tmp/ssh
 
-echo "Pansiere - Configuracoes concluidas com sucesso. Reinicie o terminal para aplicar."
+echo "Configuracoes concluidas com sucesso. #Pansiere"
